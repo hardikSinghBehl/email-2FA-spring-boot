de43cad688eca1432b4d8764da9b27eaca6d0342
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-10-09 19:08:21 +0530

    added Joke controller

---
+++ src/main/java/com/behl/salamanca/controller/JokeController.java
@@ -0,0 +1,29 @@
+package com.behl.salamanca.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.behl.salamanca.utility.JokeUtility;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class JokeController {
+
+    @GetMapping(value = "/joke", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.OK)
+    @Operation(summary = "Returns a funny joke")
+    public ResponseEntity<?> jokeRetreivalHandler(
+            @Parameter(hidden = true) @RequestHeader(name = "Authorization", required = true) final String header) {
+        return ResponseEntity.ok(JokeUtility.generate());
+    }
+
+}
