a921f61280d93a198db17b6d383965dbc87c0ca1
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-10-09 17:39:55 +0530

    init project commit

---
+++ src/main/java/com/behl/salamanca/EmailBasedTwoFactorAuthSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.behl.salamanca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailBasedTwoFactorAuthSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailBasedTwoFactorAuthSpringBootApplication.class, args);
+	}
+
+}
